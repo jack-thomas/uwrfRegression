3147839be96bccf4cd259a4929d7259af4d07927
Jack Thomas <jackthomas@ou.edu>
2016-12-05 16:17:30 -0600

    Update ts.poly

--- R/ts.poly.R
+++ R/ts.poly.R
@@ -4,24 +4,39 @@
 #' polynomial regression with the best adjusted r-squared value.
 #' @param x a vector of time series data upon which to perform polynomial regression.
 #' @param t a vector of time series sequence information.
-#' @param degree.max the maximum degree to consider for polynomial regression.
 #' @keywords ts.poly
 #' @export
 #' @examples
 #' ts.poly()
 
-ts.poly <- function(x,t=NULL,degree.max=10){
+ts.poly <- function(x,t=NULL){
   if(is.null(t)){
     t <- c(1:length(x))
   }
-  poly.r.squareds=c()
-  for (i in c(1:degree.max)){
-    poly.r.squareds[i] = summary(lm(x ~ poly(t,i, raw=TRUE)))$adj.r.squared
+  #t
+  lm.best <- lm(x ~ t)
+  r.best <- summary(lm(x ~ t))$adj.r.squared
+  for (i in c(2,3)){
+    #t^2; t^3
+    if (summary(lm(x ~ I(t^i)))$adj.r.squared > r.best){
+      lm.best <- lm(x ~ I(t^i))
+      r.best <- summary(lm.best)$adj.r.squared
+    }
+    #t + t^2; t + t^3
+    if (summary(lm(x ~ t + I(t^i)))$adj.r.squared > r.best){
+      lm.best <- lm(x ~ t + I(t^i))
+      r.best <- summary(lm.best)$adj.r.squared
+    }
   }
-  poly.best=which.max(poly.r.squareds)
-  cat("Polynomial Regression Results:")
-  cat("\n  - Best Polynomial Degree :",which.max(poly.r.squareds))
-  cat("\n  - Best Adjusted R-Squared:",
-      summary(lm(x ~ poly(t, poly.best, raw=TRUE)))$adj.r.squared)
-  invisible(lm(x ~ poly(t, poly.best, raw=TRUE)))
+  #t + t^2 + t^3
+  if (summary(lm(x ~ t + I(t^2) + I(t^3)))$adj.r.squared > r.best){
+    lm.best <- lm(x ~ t + I(t^2) + I(t^3))
+    r.best <- summary(lm.best)$adj.r.squared
+  }
+  #t^2 + t^3
+  if (summary(lm(x ~ I(t^2) + I(t^3)))$adj.r.squared > r.best){
+    lm.best <- lm(x ~ I(t^2) + I(t^3))
+    r.best <- summary(lm.best)$adj.r.squared
+  }
+  return(lm.best)
 }
