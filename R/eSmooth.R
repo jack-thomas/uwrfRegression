b5da0274b53f4ccc5c5ba18484636b4fc676df69
Jack Thomas <jackthomas@ou.edu>
2016-12-05 13:20:26 -0600

    Update eSmooth()

--- R/eSmooth.R
+++ R/eSmooth.R
@@ -1,15 +1,16 @@
 #' Simplified Exponential Smoothing
 #'
-#' This function computes an exponentially smoothed time series with a given alpha by
-#' simplifying the process.
+#' This function computes an exponentially smoothed time series with a 
+#' given alpha by simplifying the process.
 #' @param x a vector to be exponentially smoothed
+#' @param alpha Holt-Winter smoothing alpha
 #' @keywords eSmooth
 #' @export
 #' @examples
 #' eSmooth()
 
-eSmooth <- function(x){
-  hw.x <- HoltWinters(x, alpha = 0.2, beta = FALSE, gamma = FALSE)
+eSmooth <- function(x, alpha = 0.2){
+  hw.x <- HoltWinters(x, alpha = alpha, beta = FALSE, gamma = FALSE)
   es.x <- rbind(hw.x$fitted, hw.x$coefficients)
   es.x <- es.x[,2]
   return(es.x)
