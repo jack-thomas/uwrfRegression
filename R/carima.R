7dba8509540266d5d07fbbd114c5676ef202d6a0
Jack Thomas <jackthomas@ou.edu>
2016-12-11 21:56:53 -0600

    Adding functions

---
+++ R/carima.R
@@ -0,0 +1,54 @@
+#' Chatterjee Auto-Regressive Modeling
+#'
+#' This function models autoregressive processes as described by Dr. 
+#' Arunendu Chatterjee (2016.12.09).
+#' @param data a vector of time series data.
+#' @param silent whether or not to silence output.
+#' @param no.predict number of predictions to make.
+#' @keywords carima
+#' @export
+#' @examples
+#' carima()
+
+carima <- function(data, silent = FALSE, no.predict = 0){
+  boxp <- Box.test(data, type = "Ljung-Box")$p.value
+  if(boxp > 0.05){
+    cat(paste0("Not an autoregressive process. ",
+               "Ljung-Box test yielded p-value of ",
+               round(boxp,2),".\n"))
+  } else {
+    acf(data)
+    max <- readline("Based on the ACF, what is the max AR(k) to consider? ")
+    while(is.na(max) | max <= 0){
+      max <- readline("Please try again: ")
+      max <- ifelse(grepl("[^0-9]",max),-1,as.numeric(max))
+    }
+    loglikinfo <- c()
+    aicinfo <- c()
+    suggests <- 1
+    ar <- arima(data, order = c(1,0,0))
+    for (i in c(1:max)){
+      ar <- arima(data, order = c(i, 0, 0))
+      loglikinfo[i] <- ar$loglik
+      aicinfo[i] <- ar$aic
+      if (i > 1){
+        if (loglikinfo[i] > loglikinfo[i-1] && aicinfo[i] < aicinfo[i-1]){
+          suggests <- i
+        }
+      }
+    }
+    out <- data.frame(loglikinfo, aicinfo)
+    names(out) <- c("loglik","aic")
+    if(!silent){
+      print(out)
+      cat(paste0("\nSuggested: AR(",suggests,")"))
+    }
+    ar.relevant <- arima(data, order = c(suggests, 0, 0))
+    if(no.predict != 0){
+      predictions <- c(predict(ar.lake, n.ahead = 5)$pred)
+      if(!silent) cat("\n\n")
+      print(cbind(predictions))
+    }
+    invisible(ar.relevant)
+  }
+}
--- R/ctsr.R
+++ R/ctsr.R
@@ -1,4 +1,4 @@
-#' Time Series Regression
+#' Chatterjee Time Series Regression
 #'
 #' This function performs time series regression with multiple methods, then it 
 #' selects the best method based on mean absolute deviation. Once the best model
@@ -19,20 +19,34 @@ ctsr <- function(data, frequency = 1){
   mad[2] <- mean(abs(data - fitted(lm(eSmooth(data, alpha=0.4)~t))))
   mad[3] <- mean(abs(data - fitted(lm(eSmooth(data, alpha=0.6)~t))))
   mad[4] <- mean(abs(data - fitted(lm(eSmooth(data, alpha=0.8)~t))))
-#  2. Basic linear model (lm)
-  mad[5] <- mean(abs(data - fitted(lm(data~t))))
-#  3. Polynomial model (ts.poly)
-  mad[6] <- mean(abs(data - fitted(ts.poly(data))))
-#  4. Trend analysis using seasonal indices (csts)
-  mad[7] <- csts(data, frequency = frequency, mad = TRUE, df.print = FALSE)
+#  5. Basic linear model (lm)
+  mad[5] <- mean(abs(data - fitted(lm(data ~ t))))
+#  6. x ~ t^2
+  mad[6] <- mean(abs(data - fitted(lm(data ~ I(t^2)))))
+#  7. x ~ t^3  
+  mad[7] <- mean(abs(data - fitted(lm(data ~ I(t^3)))))
+#  8. x ~ t + t^2
+  mad[8] <- mean(abs(data - fitted(lm(data ~ t + I(t^2)))))
+#  9. x ~ t + t^3
+  mad[9] <- mean(abs(data - fitted(lm(data ~ t + I(t^3)))))
+# 10. x ~ t^2 + t^3
+  mad[10] <- mean(abs(data - fitted(lm(data ~ I(t^2) + I(t^3)))))
+# 11. x ~ t + t^2 + t^3
+  mad[11] <- mean(abs(data - fitted(lm(data ~ t + I(t^2) + I(t^3)))))
+# 12. Trend analysis using seasonal indices (csts)
+  mad[12] <- csts(data, frequency = frequency, mad = TRUE, df.print = FALSE)
   
   cat("MAD Values:")
-  cat("\n  Exponential Smooth (alpha = 0.2)   :", mad[1])
-  cat("\n  Exponential Smooth (alpha = 0.4)   :", mad[2])
-  cat("\n  Exponential Smooth (alpha = 0.6)   :", mad[3])
-  cat("\n  Exponential Smooth (alpha = 0.8)   :", mad[4])
-  cat("\n  Basic Linear Model (lm())          :", mad[5])
-  cat("\n  Polynomial Model (ts.poly())       :", mad[6])
-  cat("\n  Trend Analysis Using SI's (csts()) :", mad[7])
-  
+  cat(sprintf("\n  %-34s : %5.2f", "Exponential Smoothing (w = 0.2)", mad[1]))
+  cat(sprintf("\n  %-34s : %5.2f", "Exponential Smoothing (w = 0.4)", mad[2]))
+  cat(sprintf("\n  %-34s : %5.2f", "Exponential Smoothing (w = 0.6)", mad[3]))
+  cat(sprintf("\n  %-34s : %5.2f", "Exponential Smoothing (w = 0.8)", mad[4]))
+  cat(sprintf("\n  %-34s : %5.2f", "lm(x ~ t)",mad[5]))
+  cat(sprintf("\n  %-34s : %5.2f", "lm(x ~ t^2)",mad[6]))
+  cat(sprintf("\n  %-34s : %5.2f", "lm(x ~ t^3)",mad[7]))
+  cat(sprintf("\n  %-34s : %5.2f", "lm(x ~ t + t^2)",mad[8]))
+  cat(sprintf("\n  %-34s : %5.2f", "lm(x ~ t + t^3)",mad[9]))
+  cat(sprintf("\n  %-34s : %5.2f", "lm(x ~ t^2 + t^3)",mad[10]))
+  cat(sprintf("\n  %-34s : %5.2f", "lm(x ~ t + t^2 + t^3)",mad[11]))
+  cat(sprintf("\n  %-34s : %5.2f", "csts(data, frequency = n",mad[12]))
 }
